ec6a565e8caaa7862b03ad8437db7017ecde0695
Ezequiel <barbero.oe@gmail.com>
2021-01-23 01:05:26 -0300

    Finds mutations horizontally

--- mutant.go
+++ mutant.go
@@ -1,7 +1,59 @@
 package mutant
 
+// atcg
+
 // IsMutant detects mutant dna if more than four equal consecutive letters are
 // found
 func IsMutant(dna []string) bool {
+	done := make(chan struct{})
+	defer close(done)
+	horizontalSignals := findHorizontalSequences(done, dna)
+	return checkMutations(horizontalSignals)
+}
+
+func checkMutations(foundSequence <-chan struct{}) bool {
+	count := 0
+	for range foundSequence {
+		count++
+		if count > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func findHorizontalSequences(done <-chan struct{}, dna []string) <-chan struct{} {
+	out := make(chan struct{})
+	go func() {
+		defer close(out)
+		for _, row := range dna {
+			cancel := findOnRow(done, out, row)
+			if cancel {
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func findOnRow(done <-chan struct{}, out chan<- struct{}, row string) bool {
+	var base rune
+	var count int
+	for _, cell := range row {
+		select {
+		case <-done:
+			return true
+		default:
+			if base != cell {
+				base = cell
+				count = 1
+			} else {
+				count++
+				if count == 4 {
+					out <- struct{}{}
+				}
+			}
+		}
+	}
 	return false
 }
--- mutant_test.go
+++ mutant_test.go
@@ -10,6 +10,11 @@ import (
 func TestHumanDna(t *testing.T) {
 	t.Run("human-1", isHuman([]string{"A", "T"}))
 	t.Run("human-2", isHuman([]string{
+		"ATCG",
+		"TAGC",
+		"TTTT",
+		"ATCG"}))
+	t.Run("human-3", isHuman([]string{
 		"ATGCGA",
 		"CAGTGC",
 		"TTATTT",
@@ -18,8 +23,24 @@ func TestHumanDna(t *testing.T) {
 		"TCACTG"}))
 }
 
+func TestMutantDna(t *testing.T) {
+	t.Run("mutant-1", isMutant([]string{
+		"AAAA",
+		"TAGC",
+		"TTTT",
+		"ATCG"}))
+}
+
 func isHuman(dna []string) func(*testing.T) {
+	return shouldBe(false, dna)
+}
+
+func isMutant(dna []string) func(*testing.T) {
+	return shouldBe(true, dna)
+}
+
+func shouldBe(expected bool, dna []string) func(*testing.T) {
 	return func(t *testing.T) {
-		assert.Equal(t, false, IsMutant(dna))
+		assert.Equal(t, expected, IsMutant(dna))
 	}
 }
